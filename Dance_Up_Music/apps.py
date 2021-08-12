6ff3a92b0922a59e63d7d4e6870e8593484fa87e
fatimazahraBanani <fatimazahra_banani@um5.ac.ma>
2021-08-12 05:36:29 -0400

    Front end

---
+++ Dance_Up_Music/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class DanceUpMusicConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'Dance_Up_Music'
---
+++ Dance_Up_Music/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+posts = []
+# Create your views here.
+def home(request):
+    context = {'posts': posts}
+    # now the context is accessible within the template home.html
+    return render(request, 'Dance_Up_Music/home.html', context)
+
+def about(request):
+    return render(request, 'Dance_Up_Music/about.html', {'title' : 'About'})
